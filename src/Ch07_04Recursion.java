f94f84314ab9d5cf3865a67498523072bf60bbb8
animohan <anishmohan@gmail.com>
2015-05-29 05:31:27 -0700

    Ch07_04 Recursion started

---
+++ src/Ch07_04Recursion.java
@@ -0,0 +1,39 @@
+class Factorial{
+	int fact(int n){
+		int result;
+		
+		if(n==1){ 
+			return 1;}
+		else{
+			result=fact(n-1)*n;
+			return result;
+		}
+	}
+	
+}
+
+class Fibonnaci{
+	int fibo(int in){
+
+		if (in==0){return 0;}
+		if (in==1){return 1;}
+		else {
+			return (fibo(in-1)+fibo(in-2));
+			}
+	
+	}
+}
+
+class Ch07_04Recursion{
+	public static void main(String args[]){
+		
+		Factorial f= new Factorial();
+		System.out.println("Factorial of 3:"+ f.fact(3));
+		System.out.println("Factorial of 4:"+ f.fact(4));
+		System.out.println("Factorial of 5:"+ f.fact(5));
+		
+		Fibonnaci g = new Fibonnaci();
+		System.out.println("Fibonnaci numbers:"+ g.fibo(5));
+	}
+	
+}
